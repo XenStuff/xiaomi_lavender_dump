1a78bd2d9698ef43b98509a693dd188e09d4952e
xenxynon <flynryder427@gmail.com>
2022-06-24 05:25:23 +0000

    Add extras for lavender-user 10 QKQ1.190910.002 V12.5.7.0.QFGCNXM release-keys
    Signed-off-by: xenxynon <flynryder427@gmail.com>

---
+++ join_split_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cat cust/app/customized/partner-com.autonavi.minimap_12/partner-com.autonavi.minimap_12.apk.* 2>/dev/null >> cust/app/customized/partner-com.autonavi.minimap_12/partner-com.autonavi.minimap_12.apk
+rm -f cust/app/customized/partner-com.autonavi.minimap_12/partner-com.autonavi.minimap_12.apk.* 2>/dev/null
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/app/Mipay/Mipay.apk.* 2>/dev/null >> system/system/app/Mipay/Mipay.apk
+rm -f system/system/app/Mipay/Mipay.apk.* 2>/dev/null
+cat system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null >> system/system/app/VoiceAssist/VoiceAssist.apk
+rm -f system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null
+cat system/system/product/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/product/priv-app/Settings/Settings.apk
+rm -f system/system/product/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/data-app/com.eg.android.AlipayGphone_23/com.eg.android.AlipayGphone_23.apk.* 2>/dev/null >> system/system/data-app/com.eg.android.AlipayGphone_23/com.eg.android.AlipayGphone_23.apk
+rm -f system/system/data-app/com.eg.android.AlipayGphone_23/com.eg.android.AlipayGphone_23.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
